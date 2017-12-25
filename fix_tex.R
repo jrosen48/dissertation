8749aaef5d6806c3a02b9eeda6eefc04a86ec4ad
Joshua Rosenberg <jrosen@msu.edu>
2017-12-25 17:54:41 -0500

    update post

---
+++ fix_tex.R
@@ -0,0 +1,16 @@
+library(stringr)
+
+f_orig <- readLines("_book/rosenberg-dissertation.tex")
+
+start_good_content <- grep("placemarker", f_orig) + 1
+end <- length(f_orig)
+
+f_good <- f_orig[start_good_content:end]
+
+f_rep <- readLines("correct-front-matter.tex")
+
+f_out <- c(f_rep, f_good)
+
+file_conn <-file("_book/rosenberg-dissertation-mod.tex")
+writeLines(f_out, file_conn)
+close(file_conn)
