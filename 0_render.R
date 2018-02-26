a18bf600d03c3007804fae662df395cf549e3b6b
Joshua Rosenberg <jrosen@msu.edu>
2018-02-26 11:56:14 -0500

    update

--- 0_render.R
+++ 0_render.R
@@ -4,7 +4,7 @@
 rmarkdown::render_site(output_format = 'bookdown::pdf_book', encoding = 'UTF-8')
 
 # 1b. rendering site
-# rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
+rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
 
 # 2. fixing text
 source("fix_tex.R")
@@ -19,15 +19,24 @@ file.copy("_bookdown_files/rosenberg-dissertation_files",
 ### 4. MANUALLY RENDER THE FILE HERE!!!
 #########################################
 
+# this doesn't work - need a way to render to .tex to PDF with the class
+# ! Undefined control sequence.
+# \@bspredate ...\[\baselineskip ]\msu@fieldofstudy
+# ~\msu@fieldseparator ~\msu...
+# l.75 \begin
+#
+# pandoc: Error producing PDF
+# system("cd '/Users/joshuarosenberg/Google Drive/1_Research/dissertation/rosenberg-diss/docs'")
+# system("pandoc rosenberg-dissertation_mod.tex -s --variable documentclass=msu-thesis -o rosenberg-dissertation_mod.pdf")
+
+# 4b (optional). create word doc
+system("cd '/Users/joshuarosenberg/Google Drive/1_Research/dissertation/rosenberg-diss/docs'")
+system("pandoc rosenberg-dissertation_mod.tex -s --reference-docx=rosenberg-template.docx -o  rosenberg-dissertation.docx")
+
 # 5. updating github
 system("git status")
 system("git add *")
 system("git add -u")
 system("git commit -m 'update diss'")
 system("git push")
-
-# 6 (optional). create word doc
-# cd '/Users/joshuarosenberg/Google Drive/1_Research/dissertation/rosenberg-diss/docs'
-# pandoc rosenberg-dissertation_mod.tex -s -o rosenberg-dissertation.docx
-
 
