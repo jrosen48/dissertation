da6b1a0d57e0b4ca4538f0c13251815eb4fa780b
Joshua Rosenberg <jrosen@msu.edu>
2018-03-01 13:18:04 -0500

    update

--- 0_render.R
+++ 0_render.R
@@ -1,20 +1,19 @@
 #! '/Users/joshuarosenberg/Google Drive/1_Research/dissertation/rosenberg-diss/0_render.R'
 
-# 1. rendering PDF
+# 1. render PDF
 rmarkdown::render_site(output_format = 'bookdown::pdf_book', encoding = 'UTF-8')
 
-# 1b. rendering site
+# 1b. render site
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
 
-# 2. fixing text
+# 2. fix text
 source("fix_tex.R")
 
-# 3. copying cached files
+# 3. copy cached files
 if (!dir.exists("docs/rosenberg-dissertation_files")) dir.create("docs/rosenberg-dissertation_files")
-
 file.copy("_bookdown_files/rosenberg-dissertation_files", "docs", recursive=TRUE)
 
-# 4. run this line in Terminal
+# 4. convert tex to PDF
 system("cd docs; pdflatex rosenberg-dissertation_mod.tex")
 # system("find -name rosenberg-dissertation_mod.tex -execdir pdflatex {} \;")
 
@@ -23,5 +22,5 @@ system("git status; git add *; git add -u")
 system("git commit -m 'update diss'")
 system("git push")
 
-# 6. Cleaning up
+# 6. clean up
 source("0_clean-up.R")
