4f7cec92a602043241d0cd84249fe3dbba57820a
Joshua Rosenberg <jrosen@msu.edu>
2018-03-06 20:42:50 -0500

    update diss

--- 0_render.R
+++ 0_render.R
@@ -17,16 +17,18 @@ file.copy("_bookdown_files/rosenberg-dissertation_files", "docs", recursive=TRUE
 system("cd docs; pdflatex rosenberg-dissertation_mod.tex")
 
 # 5. convert tex to .docx
-# file.copy("_bookdown_files/rosenberg-dissertation_files", "docs", recursive=TRUE)
 system("cd docs;
-       pandoc rosenberg-dissertation_mod.tex --reference-docx=rosenberg-template.docx -s -o rosenberg-dissertation_mod.docx")
-
-# system("find -name rosenberg-dissertation_mod.tex -execdir pdflatex {} \;")
-
-# 5. update github
-system("git status; git add *; git add -u")
-system("git commit -m 'update diss'")
-system("git push")
+       pandoc
+       rosenberg-dissertation_mod.tex
+       --reference-docx=rosenberg-template.docx
+       -s
+       -o
+       rosenberg-dissertation_mod.docx")
 
 # 6. clean up
 source("0_clean-up.R")
+
+# 7. update github
+system("git status; git add *; git add -u")
+system("git commit -m 'update diss'")
+system("git push")
