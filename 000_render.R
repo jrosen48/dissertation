71e9d70459319fc4244dad41562ae590e24d8b84
Joshua Rosenberg <jrosen@msu.edu>
2018-07-13 21:25:36 -0400

    update diss

--- 000_render.R
+++ 000_render.R
@@ -14,11 +14,6 @@ if (!dir.exists("docs/rosenberg-dissertation_files")) dir.create("docs/rosenberg
 file.copy("_bookdown_files/rosenberg-dissertation_files", "docs", recursive=TRUE)
 
 # 4. convert tex to PDF
-
-# 1. make two figures for profiles be in the same figure tag
-# 2. add this below table 4.7, before landscape tags: \begin{flushleft}\emph{Note}. *: \emph{p} \textless{}.05; +: p \textless{} .10\end{flushleft}
-# 3. replace (B (SE)) with the proper LaTeX: $\beta(\textit SE)$
-# 4. add subscripts and note:
 # \begin{flushleft}\emph{Note}. The subscripts indicate the mean values subscripts
 # indicating values that were not statistically significantly different on
 # the basis of the ANOVA analyses.\end{flushleft}
@@ -28,13 +23,6 @@ file.copy("_bookdown_files/rosenberg-dissertation_files", "docs", recursive=TRUE
 # All moderate & 3.060 & 2.826 & 3.110 & 2.489 & 2.953$_5$\\
 # Eng. and comp. but not chall. & 3.909$_1$ & 3.487 & 3.822$_3$ & 1.276 & 3.604$_6$\\
 # Full & 3.959$_1$ & 3.801 & 3.881$_3$ & 3.742 & 3.631$_6$\\
-# for bib:
-# \makebibliographypage
-# \SingleSpacing
-# \setlength{\parindent}{-0.2in} \setlength{\leftskip}{0.2in} \noindent
-
-system("cd docs; pdflatex rosenberg-dissertation_mod.tex")
-
 # 5. convert tex to .docx
 # system("cd docs; pandoc rosenberg-dissertation_mod.tex --reference-docx=rosenberg-template.docx -s -o rosenberg-dissertation_mod.docx")
 rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')
